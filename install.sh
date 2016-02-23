c9839426cdd1ab5060ab54c88d52d781b8519ed8
Eugenio Fonseca <eugeniofonseca14@gmail.com>
2016-02-22 22:15:51 -0300

    Added color fix to the install script.

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # For license refer to https://github.com/RenatoUtsch/dotnvim/blob/master/LICENSE.txt
 
 errorMsg="I require git, curl and nvim, but one of them is not installed: ";
 errorGB=", install and come back. Aborting..."
+finished="Done. Reopen your shell and nvim."
 
 error(){
     echo >&2 $errorMsg $1 $errorGB; exit 1;
@@ -22,10 +23,20 @@ getDotNVim(){
     git clone https://github.com/RenatoUtsch/dotnvim $HOME/.config/nvim/dotnvim;
 }
 
+printColorConf(){
+    printf "\n\n# Base16 Shell\nBASE16_SHELL=\"\$HOME/.config/base16-shell/base16-default.dark.sh\"\n[[ -s \$BASE16_SHELL ]] && source \$BASE16_SHELL" >> $HOME/$1;
+}
+
+getColors(){
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell;
+    command -v zsh >/dev/null 2>&1 && printColorConf ".zshrc";
+    command -v bash >/dev/null 2>&1 && printColorConf ".bashrc";
+}
+
 config(){
     touch $HOME/.config/nvim/init.vim;
     printf "let g:settings = {}\nlet g:settings.version = 1\nsource ~/.config/nvim/dotnvim/init.vim\n" > $HOME/.config/nvim/init.vim;
-    nvim "+PlugInstall +qall"
+    nvim "+PlugInstall" "+qall"
 }
 
 existsOrFail "git"
@@ -33,4 +44,6 @@ existsOrFail "curl"
 existsOrFail "nvim"
 getPlugVim
 getDotNVim
+getColors
 config
+echo $finished
